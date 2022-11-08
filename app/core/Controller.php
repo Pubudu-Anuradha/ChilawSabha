dd2c2ce5c2410ce7c7369443fe2464a07a3daa50
TharinduSampath2000 <stharindu444@gmail.com>
2022-11-08 15:43:02 +0530

    Created the parent controller for all future child controllers with 2 functions to return an instance of a model and render a view with data

---
+++ app/core/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+class Controller
+{
+
+    //A function for child classes to get an instance of a defined model
+    public function model($model)
+    {
+        require_once '../models/' . $model . '.php';
+        return new $model();
+    }
+
+    //Render a defined view and pass the data provided by the caller
+    public function view($view, $data = [])
+    {
+        require_once '../views/' . $view . '.php'; 
+    }
+}
