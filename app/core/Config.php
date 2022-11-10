871a547a5c58e1c5660b2182c88a259e580ae233
TharinduSampath2000 <stharindu444@gmail.com>
2022-11-10 12:52:22 +0530

    Changed mistakes with paths

--- app/core/Config.php
+++ app/core/Config.php
@@ -2,6 +2,6 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'ChilawSabha');
-define('URLROOT', 'http://localhost/ChilawSabha/public');
+define('DB_NAME', 'chilawsabha');
+define('URLROOT', 'http://localhost/ChilawSabha');
 
--- app/core/Controller.php
+++ app/core/Controller.php
@@ -6,13 +6,13 @@ class Controller
     //A function for child classes to get an instance of a defined model
     public function model($model)
     {
-        require_once '../models/' . $model . '.php';
+        require_once 'app/models/' . $model . '.php';
         return new $model();
     }
 
     //Render a defined view and pass the data provided by the caller
     public function view($view, $data = [])
     {
-        require_once '../views/' . $view . '.php'; 
+        require_once 'app/views/' . $view . '.php'; 
     }
 }
