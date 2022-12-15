ba5903ecfb9def6f73e19954ba4c22c7d019e0e5
sandarud99 <2020cs046@stu.ucsc.cmb.ac.lk>
2022-12-15 23:11:07 +0530

    change styles

--- app/views/Complaint/newComplaint.php
+++ app/views/Complaint/newComplaint.php
@@ -6,6 +6,29 @@ $complaint = $data['complaint'];
 if ($complaint->num_rows == 0) {
     echo "NO SUCH COMPLAINT";
 } else {
-    $complaint = $complaint->fetch_assoc();
-    var_dump($complaint);
+    if (isset($data['complaint'])) {
+        $complaint = $data['complaint']->fetch_assoc();
+        ?>
+
+
+        <div class="one-complaint">
+            <div class="status-set">
+                <p>Status</p>
+                <p class="new">New</p>
+                <button>Accept</button>
+            </div>
+
+            <div class="complaint-data">
+                id : <?=$complaint['complaint_id']?><br>
+                name : <?=$complaint['name']?><br>
+                email : <?=$complaint['email']?><br>
+                number : <?=$complaint['mobi_num']?><br>
+                date : <?=$complaint['date']?><br>
+                category : <?=$complaint['category']?><br>
+                message : <?=$complaint['message']?><br>
+            </div>
+
+        </div>
+<?php
+}
 }
