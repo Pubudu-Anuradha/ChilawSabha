ca6cdbd68cd3aaa5fe9597cc47335de9978e228c
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2023-02-01 09:31:56 +0530

    reformatted pagination testpage

--- app/views/Home/pagination.php
+++ app/views/Home/pagination.php
@@ -11,8 +11,8 @@
         </tr>
         <?php
         $table = $data['test'];
-        if (!$table['error'] && !$table['nodata']) {
-            foreach ($table['result'] as $row) { ?>
+        if (!$table['error'] && !$table['nodata']) :
+            foreach ($table['result'] as $row) : ?>
                 <tr>
                     <td>
                         <?= $row['id'] ?>
@@ -27,47 +27,49 @@
                         <?= $row['age'] ?>
                     </td>
                 </tr>
-            <?php }
-        } else { ?>
+            <?php endforeach;
+        else :  ?>
             <tr>
                 <td colspan="2">No Data</td>
             </tr>
-        <?php } ?>
+        <?php endif; ?>
 
     </table>
     <?php
     $size = $table['page'][1];
     $max = $table['count'];
-    $page_count = ceil($max / $size);
+    $page_count = ceil($max / $size); ?>
 
-    if ($page_count > 4) { ?>
-        <script>
-            const changePage = () => {
-                document.getElementById("pageForm").submit();
-            }
-        </script>
-        <form action="<?= URLROOT . "/Home/paginationTest" ?>" method="get" id="pageForm">
+    <script>
+        const changePage = () => {
+            document.getElementById("pageForm").submit();
+        }
+    </script>
+    <form action="<?= URLROOT . "/Home/paginationTest" ?>" method="get" id="pageForm">
+        <?php
+        if ($page_count > 4) : ?>
             <a href="<?= URLROOT . "/Home/paginationTest?page=0&size=$size" ?>">1</a>
             <a href="<?= URLROOT . "/Home/paginationTest?page=1&size=$size" ?>">2</a>
             <select name="page" onchange="changePage()" id="page">
                 <?php
                 $i = 0;
-                for (; $i * $size < $max; $i++) { ?>
+                for (; $i * $size < $max; $i++) : ?>
                     <option value="<?= $i ?>" <?php if ($i == $table['page'][0] / $size) echo "selected" ?>><?= $i + 1 ?></option>
-                <?php } ?>
+                <?php endfor ?>
             </select>
             <a href="<?= URLROOT . "/Home/paginationTest?page=" . ($page_count - 2) . "&size=$size" ?>"><?= $page_count - 1 ?></a>
-            <a href="<?= URLROOT . "/Home/paginationTest?page=" . ($page_count - 1) . "&size=$size" ?>"><?= $page_count ?></a> <br>
-            No.of Rows : <select name="size" onchange="changePage()" id="size">
-                <?php foreach ([10, 25, 50, 100] as $page_size) : ?>
-                    <option value="<?= $page_size ?>" <?php if ($page_size == $size) echo "selected" ?>><?= $page_size ?></option>
-                <?php endforeach; ?>
-            </select>
-        </form>
+            <a href="<?= URLROOT . "/Home/paginationTest?page=" . ($page_count - 1) . "&size=$size" ?>"><?= $page_count ?></a>
+            <?php else :
+            for ($i = 0; $i * $size < $max; $i++) : ?>
+                <a href="<?= URLROOT . "/Home/paginationTest?page=$i&size=$size" ?>"><?= $i + 1 ?></a>
         <?php
-    } else {
-        for ($i = 0; $i * $size < $max; $i++) { ?>
-            <a href="<?= URLROOT . "/Home/paginationTest?page=$i&size=$size" ?>"><?= $i + 1 ?></a>
-    <?php }
-    } ?>
+            endfor;
+        endif; ?>
+        No.of Rows : <select name="size" onchange="changePage()" id="size">
+            <?php foreach ([10, 25, 50, 100] as $page_size) : ?>
+                <option value="<?= $page_size ?>" <?php if ($page_size == $size) echo "selected" ?>><?= $page_size ?></option>
+            <?php endforeach; ?>
+            <option value="<?= $page_size ?>" <?php if ($page_size == $size) echo "selected" ?>><?= $page_size ?></option>
+        </select>
+    </form>
 </div>
