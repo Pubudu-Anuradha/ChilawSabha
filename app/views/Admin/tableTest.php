a6ed45f7446627f3c6b95b6921f69ebf3113e450
sandarud99 <2020cs046@stu.ucsc.cmb.ac.lk>
2023-02-02 10:47:09 +0530

    Change table stylings

--- app/views/Admin/tableTest.php
+++ app/views/Admin/tableTest.php
@@ -1,53 +1,44 @@
-
-<div class="body">
-    <div class="b-left">
-        <h1>Side Bar</h1>
+<div class="content">
+    <div class="content-table">
+        <table>
+            <thead>
+                <tr>
+                    <th>Accestion number</th>
+                    <th>Title</th>
+                    <th>Author</th>
+                    <th>Publisher</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>P305</td>
+                    <td>Harry Potter</td>
+                    <td>J.K. Rowling</td>
+                    <td>Animus Klado</td>
+                    <td>
+                        <div  class="btn-column">
+                            <button class="btn edit">Edit</button>
+                            <button class="btn lost">Lost</button>
+                            <button class="btn delist">Delist</button>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>A45</td>
+                    <td>Atomic Habits</td>
+                    <td>James Clear</td>
+                    <td>Penguin Random</td>
+                    <td>
+                        <div class="btn-column">
+                            <button class="btn edit">Edit</button>
+                            <button class="btn lost">Lost</button>
+                            <button class="btn delist">Delist</button>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     </div>
-
-    <div class="b-right">
-
-            <div class="content-table">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Accestion number</th>
-                            <th>Title</th>
-                            <th>Author</th>
-                            <th>Publisher</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>P305</td>
-                            <td>Harry Potter</td>
-                            <td>J.K. Rowling</td>
-                            <td>Animus Klado</td>
-                            <td>
-                                <div class="btn">
-                                    <button class="btn-edit">Edit</button>
-                                    <button class="btn-lost">Lost</button>
-                                    <button class="btn-delist">Delist</button>
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>A45</td>
-                            <td>Atomic Habits</td>
-                            <td>James Clear</td>
-                            <td>Penguin Random</td>
-                            <td>
-                                <div class="btn">
-                                    <button class="btn-edit">Edit</button>
-                                    <button class="btn-lost">Lost</button>
-                                    <button class="btn-delist">Delist</button>
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-    </div>
-
 
 </div>
