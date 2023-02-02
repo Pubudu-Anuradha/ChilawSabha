0220a2611b1ba1350f46df0d58c7328b8f072ad5
sandarud99 <2020cs046@stu.ucsc.cmb.ac.lk>
2023-02-02 10:19:54 +0530

    Added table test page and table stylings

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -2,13 +2,18 @@
 
 class Admin extends Controller
 {
-    public function __construct()
-    {
-        $this->authenticateRole('Admin');
-    }
+    // public function __construct()
+    // {
+    //     $this->authenticateRole('Admin');
+    // }
 
     public function index()
     {
         $this->view('Admin/index');
+    }
+
+    public function tableTest()
+    {
+        $this->view('Admin/tableTest', 'Tables', [], ['main', 'table']);
     }
 }
---
+++ app/views/Admin/tableTest.php
@@ -0,0 +1,53 @@
+
+<div class="body">
+    <div class="b-left">
+        <h1>Side Bar</h1>
+    </div>
+
+    <div class="b-right">
+
+            <div class="content-table">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Accestion number</th>
+                            <th>Title</th>
+                            <th>Author</th>
+                            <th>Publisher</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>P305</td>
+                            <td>Harry Potter</td>
+                            <td>J.K. Rowling</td>
+                            <td>Animus Klado</td>
+                            <td>
+                                <div class="btn">
+                                    <button class="btn-edit">Edit</button>
+                                    <button class="btn-lost">Lost</button>
+                                    <button class="btn-delist">Delist</button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>A45</td>
+                            <td>Atomic Habits</td>
+                            <td>James Clear</td>
+                            <td>Penguin Random</td>
+                            <td>
+                                <div class="btn">
+                                    <button class="btn-edit">Edit</button>
+                                    <button class="btn-lost">Lost</button>
+                                    <button class="btn-delist">Delist</button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+    </div>
+
+
+</div>
