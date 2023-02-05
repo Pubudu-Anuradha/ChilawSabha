1b88265f3ee745492be1abb7792ae6fe90c83ba2
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2023-02-05 18:06:05 +0530

    Added page naviagtion part adn its styles

--- app/views/Posts/index.php
+++ app/views/Posts/index.php
@@ -35,7 +35,7 @@ $posts = [
             <label for="search">
                 Search
             </label>
-            <input class="search" type="search" name="search" id="search" value="<?= isset($_GET['search'])?$_GET['search']:''?>">
+            <input class="search" type="search" name="search" id="search" value="<?= isset($_GET['search']) ? $_GET['search']:''?>">
             <span onclick="send()"></span>
         </div>
         <div class="filter">
@@ -44,7 +44,7 @@ $posts = [
             </label>
             <select onchange="send()" name="category" id="category">
                 <?php foreach (['All'=>'All','test1'=>'Test Category 1','test2'=> 'Test Cat 2'] as $cat_v=>$cat_d):?>
-                    <option value="<?=$cat_v?>" <?php if(isset($_GET['category']) && $_GET['category']==$cat_v) echo 'selected' ?>>
+                    <option value="<?=$cat_v?>" <?php if(isset($_GET['category']) && $_GET['category']==$cat_v) {echo 'selected';} ?>>
                         <?=$cat_d?>
                     </option>
                 <?php endforeach; ?>
@@ -56,13 +56,12 @@ $posts = [
             </label>
             <select onchange="send()" name="sort" id="sort">
                 <?php foreach(['DESC'=>'Newest to Oldest','ASC'=>'Oldest to Newest'] as $val=>$desc):?>
-                    <option value="<?= $val ?>" <?php if(isset($_GET['sort']) && $_GET['sort']==$val) echo 'selected' ?>>
+                    <option value="<?= $val ?>" <?php if(isset($_GET['sort']) && $_GET['sort']==$val) {echo 'selected';} ?>>
                         <?=$desc?>
                     </option>
                 <?php endforeach; ?>
             </select>
         </div>
-    </form>
 </div>
 <div class="posts">
     <?php foreach($posts as $post): ?>
@@ -96,3 +95,41 @@ $posts = [
         document.getElementById('filterform').submit();
     }
 </script>
+<div class="page-nav">
+    <?php
+    // $size = $table['page'][1];
+    // $max = $table['count'];
+    // $page = $table['page];
+    //placeholders
+    $page = [20,10];
+    $size = $page[1];
+    $max = 101;
+    $page_count = ceil($max / $size);
+    $current = $page[0] / $size;
+    ?>
+    <div class="page-nos">
+        <?php if($current!=0):?>
+            <a href="<?= URLROOT . "/Posts/?page=0&size=$size" ?>" class="page-btn first"></a>
+            <a href="<?= URLROOT . "/Posts/?page=".($current - 1)."&size=$size" ?>" class="page-btn prev"></a>
+        <?php endif; ?>
+        <select name="page" onchange="send()" id="page">
+            <?php
+            $i = 0;
+            for (; $i * $size < $max; $i++) : ?>
+                <option value="<?= $i ?>" <?= $i==$current?'selected' : ''?>><?= $i + 1 ?></option>
+            <?php endfor ?>
+        </select>
+        <?php if($current<$page_count-1):?>
+            <a href="<?= URLROOT . "/Posts/?page=" . ($current + 1) . "&size=$size" ?>" class="page-btn next"></a>
+            <a href="<?= URLROOT . "/Posts/?page=" . ($page_count - 1) . "&size=$size" ?>" class="page-btn last"></a>
+        <?php endif; ?>
+    </div>
+    <div class="page-size">
+        No.of Posts per page : <select name="size" onchange="send()" id="size">
+            <?php foreach ([10, 25, 50, 100] as $page_size) : ?>
+                <option value="<?= $page_size ?>" <?= $page_size == $size?'selected':''?>><?= $page_size ?></option>
+            <?php endforeach; ?>
+        </select>
+    </div>
+</div>
+</form>
