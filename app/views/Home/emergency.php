3e933b6c36e9f3d16d6e3a11e0454d413ddb753e
TharinduSampath2000 <stharindu444@gmail.com>
2023-02-09 21:25:35 +0530

    Added styles to emergency details view

--- app/views/Home/emergency.php
+++ app/views/Home/emergency.php
@@ -8,15 +8,17 @@
     $emergency_details = !$data['emergency_details']['error'] && !$data['emergency_details']['nodata'] ? $data['emergency_details']['result']:false;
     $prev_detail=null;
     if($emergency_details):
-      foreach($emergency_details as $emergency_detail): 
+      foreach($emergency_details as $emergency_detail):
+
         if($prev_detail==null or $emergency_detail['category_name']!=$prev_detail['category_name']):
           echo '<h2>' . $emergency_detail['category_name'] . '</h2>';
         endif;
-
-        echo '<h3>' . $emergency_detail['place_name'] . '</h3>';
-        echo '<p>Telephone: 0' . $emergency_detail['tel'] . '</p>';
-        echo '<p>Address: ' . $emergency_detail['place_address'] . '</p>';
-        $prev_detail = $emergency_detail;
+        echo '<div class="emergency-detail">';
+          echo '<h3>' . $emergency_detail['place_name'] . '</h3>';
+          echo '<p>Telephone: 0' . $emergency_detail['tel'] . '</p>';
+          echo '<p>Address: ' . $emergency_detail['place_address'] . '</p>';
+          $prev_detail = $emergency_detail;
+        echo '</div>';
       endforeach;
     endif;
   ?>
