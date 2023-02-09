26111e2595c747bdd1018bb1c013ba92afac1d36
Hasala <hasaladissanayake@yahoo.com>
2023-02-09 18:05:56 +0530

    added suitable icons for sidebar for all 4 users

--- app/views/LibraryStaff/Sidebar.php
+++ app/views/LibraryStaff/Sidebar.php
@@ -1,7 +1,7 @@
 <div class="sidebar">
 
     <div class="items"><a href="<?=URLROOT . '/LibraryStaff/index' ?>" class="sidebar-drop"><img src="<?=URLROOT . '/public/assets/dashboard.png'?>" alt="sidebar-image" class="sidebar-img"><span> Dashboard </span></a></div>
-    <div class="items"><a href="#" class="sidebar-drop"><img src="<?=URLROOT . '/public/assets/service.png'?>" alt="sidebar-image" class="sidebar-img"><span onclick="window.location.href = '<?=URLROOT . '/LibraryStaff/Bookcatalog'?>'"> Book Catalogue </span><span class="see-more" onclick='expandSideBar("sub-items-serv")'>	&#43;</span></a>
+    <div class="items"><a href="#" class="sidebar-drop"><img src="<?=URLROOT . '/public/assets/bookcatalog.png'?>" alt="sidebar-image" class="sidebar-img"><span onclick="window.location.href = '<?=URLROOT . '/LibraryStaff/Bookcatalog'?>'"> Book Catalogue </span><span class="see-more" onclick='expandSideBar("sub-items-serv")'>	&#43;</span></a>
         <div class="sub-items" id="sub-items-serv">
             <a href="<?=URLROOT . '/LibraryStaff/addbooks' ?>">Add Book</a>
             <a href="<?=URLROOT . '/LibraryStaff/lostbooks' ?>">Lost Book List</a>
@@ -10,14 +10,14 @@
 
         </div>
     </div>
-    <div class="items"><a href="<?=URLROOT . '/LibraryStaff/bookrequest' ?>" class="sidebar-drop"><img src="<?=URLROOT . '/public/assets/event.png'?>" alt="sidebar-image" class="sidebar-img"><span> Book Requests </span></a></div>
+    <div class="items"><a href="<?=URLROOT . '/LibraryStaff/bookrequest' ?>" class="sidebar-drop"><img src="<?=URLROOT . '/public/assets/bookrequest.png'?>" alt="sidebar-image" class="sidebar-img"><span> Book Requests </span></a></div>
     <div class="items"><a href="#" class="sidebar-drop"><img src="<?=URLROOT . '/public/assets/usermanagement.png'?>" alt="sidebar-image" class="sidebar-img"><span onclick="window.location.href = '<?=URLROOT . '/LibraryStaff/Users'?>'"> User Management </span><span class="see-more" onclick='expandSideBar("sub-items-user")'>	&#43;</span></a>
         <div class="sub-items" id="sub-items-user">
             <a href="<?=URLROOT . '/LibraryStaff/addusers' ?>">Create User</a>
             <a href="<?=URLROOT . '/LibraryStaff/disabledusers' ?>">Disabled User List</a>
         </div>
     </div>
-    <div class="items"><a href="<?=URLROOT . '/LibraryStaff/analytics' ?>" class="sidebar-drop"><img src="<?=URLROOT . '/public/assets/project.png'?>" alt="sidebar-image" class="sidebar-img"><span> Analytics </span></a></div>
+    <div class="items"><a href="<?=URLROOT . '/LibraryStaff/analytics' ?>" class="sidebar-drop"><img src="<?=URLROOT . '/public/assets/analytics.png'?>" alt="sidebar-image" class="sidebar-img"><span> Analytics </span></a></div>
 
 </div>
 
