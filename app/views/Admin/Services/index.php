a2ea5eb191a9ac5228684bc62eb608e9612cc1e9
Hasala <hasaladissanayake@yahoo.com>
2023-02-10 08:17:17 +0530

    added admin tables

--- app/views/Admin/Services/index.php
+++ app/views/Admin/Services/index.php
@@ -3,12 +3,13 @@
         Manage Services
     </h1>
     <hr>
+<div class="filters">
     <form method="get" id="filterform">
         <div class="filter">
             <label for="search">
                 Search
             </label>
-            <input class="search" type="search" name="search" id="search" value="<?= isset($_GET['search']) ? $_GET['search']:''?>">
+            <input class="search" type="search" name="search" id="search" value="<?=isset($_GET['search']) ? $_GET['search'] : ''?>">
             <span onclick="send()"></span>
         </div>
         <div class="filter">
@@ -16,11 +17,11 @@
                 Filter by Category
             </label>
             <select onchange="send()" name="category" id="category">
-                <?php foreach (['All','Financial','Government','Tender'] as $cat):?>
-                    <option value="<?=$cat?>" <?php if(isset($_GET['category']) && $_GET['category']==$cat) {echo 'selected';} ?>>
+                <?php foreach (['All', 'Financial', 'Government', 'Tender'] as $cat): ?>
+                    <option value="<?=$cat?>" <?php if (isset($_GET['category']) && $_GET['category'] == $cat) {echo 'selected';}?>>
                         <?=$cat?>
                     </option>
-                <?php endforeach; ?>
+                <?php endforeach;?>
             </select>
         </div>
         <div class="filter">
@@ -28,13 +29,14 @@
                 Sort by date
             </label>
             <select onchange="send()" name="sort" id="sort">
-                <?php foreach(['DESC'=>'Newest to Oldest','ASC'=>'Oldest to Newest'] as $val=>$desc):?>
-                    <option value="<?= $val ?>" <?php if(isset($_GET['sort']) && $_GET['sort']==$val) {echo 'selected';} ?>>
+                <?php foreach (['DESC' => 'Newest to Oldest', 'ASC' => 'Oldest to Newest'] as $val => $desc): ?>
+                    <option value="<?=$val?>" <?php if (isset($_GET['sort']) && $_GET['sort'] == $val) {echo 'selected';}?>>
                         <?=$desc?>
                     </option>
-                <?php endforeach; ?>
+                <?php endforeach;?>
             </select>
         </div>
+</div>
 <div class="content-table">
     <table>
         <thead>
@@ -47,12 +49,11 @@
             </tr>
         </thead>
         <tbody>
-            <?php for($i=0;$i<=25;++$i): ?>
+            <?php for ($i = 0; $i <= 25; ++$i): ?>
             <tr>
-                <td><?= $i ?></td>
-                <td>Service <?= $i ?></td>
+                <td><?=$i?></td>
+                <td>Service <?=$i?></td>
                 <td>Infrastructure</td>
-                <td>2020-13-13</td>
                 <td>0325681285</td>
                 <td>
                     <div  class="btn-column">
@@ -68,34 +69,34 @@
 
 <div class="page-nav">
     <?php
-    $page = [0,10];
-    $size = $page[1];
-    $max = 100;
-    $page_count = ceil($max / $size);
-    $current = $page[0] / $size;
-    ?>
+$page = [0, 10];
+$size = $page[1];
+$max = 100;
+$page_count = ceil($max / $size);
+$current = $page[0] / $size;
+?>
     <div class="page-nos">
-        <?php if($current!=0):?>
+        <?php if ($current != 0): ?>
             <a href="#" class="page-btn">&lt;&lt;</a>
             <a href="#" class="page-btn">&lt;</a>
-        <?php endif; ?>
+        <?php endif;?>
         <select name="page" onchange="send()" id="page">
             <?php
-            $i = 0;
-            for (; $i * $size < $max; $i++) : ?>
-                <option value="<?= $i ?>" <?= $i==$current?'selected' : ''?>><?= $i + 1 ?></option>
-            <?php endfor ?>
+$i = 0;
+for (; $i * $size < $max; $i++): ?>
+                <option value="<?=$i?>" <?=$i == $current ? 'selected' : ''?>><?=$i + 1?></option>
+            <?php endfor?>
         </select>
-        <?php if($current<$page_count-1):?>
+        <?php if ($current < $page_count - 1): ?>
             <a href="#" class="page-btn">&gt;</a>
             <a href="#" class="page-btn">&gt;&gt;</a>
-        <?php endif; ?>
+        <?php endif;?>
     </div>
     <div class="page-size">
         No.of Posts per page : <select name="size" onchange="send()" id="size">
-            <?php foreach ([10, 25, 50, 100] as $page_size) : ?>
-                <option value="<?= $page_size ?>" <?= $page_size == $size?'selected':''?>><?= $page_size ?></option>
-            <?php endforeach; ?>
+            <?php foreach ([10, 25, 50, 100] as $page_size): ?>
+                <option value="<?=$page_size?>" <?=$page_size == $size ? 'selected' : ''?>><?=$page_size?></option>
+            <?php endforeach;?>
         </select>
     </div>
 </div>
