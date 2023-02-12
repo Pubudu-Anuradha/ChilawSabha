c324e1c0af42646c9f7fd286be0745370a873cc5
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2023-02-12 19:35:55 +0530

    Added Select method for group component set

--- app/components/Input/Group.php
+++ app/components/Input/Group.php
@@ -1,1 +1,24 @@
 <?php 
+class Group {
+  public static function select(
+    $title,$name,$options,$id=NULL,$class=NULL,$selected=NULL,$required=true
+    // Options must be an associative array
+  ){?>
+        <!-- $title,$name, $id = null, $class = null,$placeholder=NULL,$value=NULL,$required=true,$type=NULL -->
+  <div class="inputfield">
+    <label for="<?= $name ?>"><?= $title ?></label>
+    <select name="<?= $name ?>"
+            <?=$id ? 'id="' . $id . '"' : ''?>
+            <?=$class ?'class="' . $class . '"' : ''?>
+            <?= $required? 'required' : ''?>
+    >
+    <?php foreach($options as $value=>$name): ?>
+      <option value="<?=$value?>" <?= $selected && $selected==$value ? 'selected':'' ?>>
+          <?=$name?>
+      </option>
+    <?php endforeach; ?>
+    </select>
+  </div>
+  <?php
+  }
+}
--- app/views/Components/index.php
+++ app/views/Components/index.php
@@ -3,3 +3,11 @@ Text::text('Enter your name','name','name-id','name-in','Please Enter your name 
 Text::email('Enter your Email','email');
 Text::password('Enter your password','password');
 Text::textarea('Enter your message','message','message-id','message-in','Please Enter your message here','Saman Kumara is okay');
+
+Group::select('selection','Select your word',[
+    'a'=>'Apple',
+    'b'=>'Ball',
+    'c'=>'Cat',
+    'd'=>'Dog',
+    'e'=>'Elephant',
+],selected:'d');
