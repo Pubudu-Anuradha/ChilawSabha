1319767764b8f9fe62b31d015d3a9f05fd2ab9af
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2023-02-12 19:22:50 +0530

    Added method to make reusable components, added first input components

---
+++ app/components/Input/Text.php
@@ -0,0 +1,57 @@
+<?php
+
+use Text as GlobalText;
+
+class Text
+{
+    public static function text(
+        $title,$name, $id = null, $class = null,$placeholder=NULL,$value=NULL,$required=true,$type=NULL
+    ) { ?>
+    <div class="inputfield">
+        <label for="<?= $name ?>">
+            <?= $title ?>    
+        </label>
+        <input type="<?= $type ? $type : 'text'?>" 
+            name="<?=$name?>"
+            <?=$id ? 'id="' . $id . '"' : ''?>
+            <?=$class ?'class="' . $class . '"' : ''?>
+            <?=$placeholder ?'placeholder="' . $placeholder . '"' : ''?>
+            <?=$value ?'value="' . $value . '"' : ''?>
+            <?=$required ?'required' : ''?>
+        />
+    </div>
+    <?php
+    }
+
+    public static function password(
+        $title,$name, $id = null, $class = null,$required=true,$value=NULL,$placeholder=NULL
+    ) { 
+        Text::text($title,$name,$id,$class,$placeholder,$value,$required,'password');
+    }
+
+    public static function email(
+        $title,$name, $id = null, $class = null,$required=true,$value=NULL,$placeholder=NULL
+    ) { 
+        Text::text($title,$name,$id,$class,$placeholder,$value,$required,'email');
+    }
+    
+    public static function textarea(
+        $title,$name, $id = null, $class = null,$placeholder=NULL,$value=NULL,$rows=10,$cols=30,$required=true
+    ) { ?>
+    <div class="inputfield">
+        <label for="<?= $name ?>">
+            <?= $title ?>    
+        </label>
+        <textarea 
+            name="<?=$name?>"
+            <?=$id ? 'id="' . $id . '"' : ''?>
+            <?=$class ?'class="' . $class . '"' : ''?>
+            <?=$placeholder ?'placeholder="' . $placeholder . '"' : ''?>
+            <?="rows=\"$rows\""?>
+            <?="cols=\"$cols\""?>
+            <?=$required ?'required' : ''?>
+        ><?=$value ?$value: ''?></textarea>
+    </div>
+    <?php
+    }
+}
---
+++ app/views/Components/index.php
@@ -0,0 +1,5 @@
+<?php
+Text::text('Enter your name','name','name-id','name-in','Please Enter your name here','Saman Kumara');
+Text::email('Enter your Email','email');
+Text::password('Enter your password','password');
+Text::textarea('Enter your message','message','message-id','message-in','Please Enter your message here','Saman Kumara is okay');
