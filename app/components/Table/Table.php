7316eb79dc0ef86f8ec94a1b0da6acd395ede00b
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2023-02-19 11:26:28 +0530

    Merged Head and Body methods in Table to one Table function

--- app/components/Table/Table.php
+++ app/components/Table/Table.php
@@ -1,32 +1,6 @@
 <?php
 class Table{
-
-  public static function Head($columns,$caption = NULL,$actions = true,$actions_singular=false){
-  ?>
-  <div class="content-table">
-    <table>
-      <?php if($caption):?>
-      <caption><?= $caption ?></caption>
-      <?php endif; ?>
-      <thead>
-        <tr>
-          <?php foreach($columns as $column):?>
-            <th>
-              <?=$column?>
-            </th>
-          <?php endforeach;
-          if($actions):
-            if($actions_singular):?>
-              <th>Action</th>
-            <?php else: ?>
-              <th>Actions</th>
-            <?php endif;
-            endif;?>
-        </tr>
-      </thead>
- <?php }
-  public static function Body($columns,$row_data,$actions,
-  $action_funcs = [
+  public static function Table($columns,$row_data,$caption = NULL,$actions = [
     'Action 1' => [
     ['F string %s %s', 'id','name'],// sprintf arguments "%s string" followed by column names
     // contents of column will be used as actual arguments.
@@ -37,19 +11,41 @@ class Table{
     'view bg-green'
     ],
   ]){
+    $action_count = count($actions);
   ?>
+  <div class="content-table">
+    <table>
+      <?php if($caption):?>
+      <caption><?= $caption ?></caption>
+      <?php endif; ?>
+      <thead>
+        <tr>
+          <?php foreach($columns as $name => $title):?>
+            <th>
+              <?=$title?>
+            </th>
+          <?php endforeach;
+          if($action_count>0):
+            if($action_count==1):?>
+              <th>Action</th>
+            <?php else: ?>
+              <th>Actions</th>
+            <?php endif;
+            endif;?>
+        </tr>
+      </thead>
       <tbody>
         <?php foreach($row_data as $row):?>
           <tr>
-            <?php foreach($columns as $column): ?>
+            <?php foreach($columns as $col_name => $title): ?>
               <td>
-                <?= $row[$column] ?>
+                <?= $row[$col_name] ?>
               </td>
             <?php endforeach; ?>
-            <?php if($actions): ?>
+            <?php if($action_count>0): ?>
               <td>
                 <div class="btn-column">
-             <?php foreach($action_funcs as $name => $func):
+             <?php foreach($actions as $name => $func):
                 // Converting each index to use referenced column's value
                 for($i = 1;$i < count($func[0]);++$i){
                   $func[0][$i] = $row[$func[0][$i]];
@@ -69,5 +65,4 @@ class Table{
     </table>
   </div>
  <?php }
-
 }
--- app/views/Components/table.php
+++ app/views/Components/table.php
@@ -1,15 +1,14 @@
 <?php
-Table::Head(['id','name','Age'],'Test Table');
-Table::Body(['id','name','Age'],[
-  ['id' => 1 , 'name' => 'John', 'Age' => 32],
-  ['id' => 2 , 'name' => 'John', 'Age' => 32],
-  ['id' => 3 , 'name' => 'John', 'Age' => 32],
-  ['id' => 4 , 'name' => 'John', 'Age' => 32],
-  ['id' => 5 , 'name' => 'John', 'Age' => 32],
-  ['id' => 6 , 'name' => 'John', 'Age' => 32],
-  ['id' => 7 , 'name' => 'John', 'Age' => 32],
-  ['id' => 8 , 'name' => 'John', 'Age' => 32],
-  ['id' => 9 , 'name' => 'John', 'Age' => 32],
-  ['id' => 10 , 'name' => 'John', 'Age' => 32],
-  ['id' => 11 , 'name' => 'John', 'Age' => 32],
-],true);
+Table::Table(['id'=>'id','name'=>'Name','age'=>'Age'],[
+  ['id' => 1 , 'name' => 'John', 'age' => 32],
+  ['id' => 2 , 'name' => 'John', 'age' => 32],
+  ['id' => 3 , 'name' => 'John', 'age' => 32],
+  ['id' => 4 , 'name' => 'John', 'age' => 32],
+  ['id' => 5 , 'name' => 'John', 'age' => 32],
+  ['id' => 6 , 'name' => 'John', 'age' => 32],
+  ['id' => 7 , 'name' => 'John', 'age' => 32],
+  ['id' => 8 , 'name' => 'John', 'age' => 32],
+  ['id' => 9 , 'name' => 'John', 'age' => 32],
+  ['id' => 10 , 'name' => 'John', 'age' => 32],
+  ['id' => 11 , 'name' => 'John', 'age' => 32],
+],'Test Table');
