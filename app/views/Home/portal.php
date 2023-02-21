ebbd78ba0cefb056f455ada67192fcf284c801b4
Hasala <hasaladissanayake@yahoo.com>
2023-02-21 17:30:04 +0530

    changed the format and some styles in library portal page,added assets needed for that page

--- app/views/Home/portal.php
+++ app/views/Home/portal.php
@@ -4,50 +4,55 @@
 </div>
 
 <div class="slideshow">
-
     <img src="" alt="slideshow" id="slideshow">
 
     <button  class="slideshow-btn left-btn" onclick='prev(["<?=URLROOT . "/public/assets/sabha1.jpg"?>","<?=URLROOT . "/public/assets/sabha2.jpg"?>"])'>&#10094;</button>
     <button  class="slideshow-btn right-btn" onclick='next(["<?=URLROOT . "/public/assets/sabha1.jpg"?>","<?=URLROOT . "/public/assets/sabha2.jpg"?>"])'>&#10095;</button>
+</div>
+
+<div class="portal-main">
+
+  <div class="portal-content">
+
+    <div class="portal-intro">
+      <p >Welcome to the Chilaw Public Library Online Portal. 
+      The library has more than 10,000 books that you can search and find copies. 
+      If you are a library member you can log in to your dashboard and access many other features. 
+      Contact the Librarian if there is any problem or Technical error.</p>
+    </div>
+
+    <div class="portal-buttonsContainer">
+      <div class="button-container">
+        <img src="<?= URLROOT . '/public/assets/open-book.png' ?>" class="portal-button-icons" alt="book">
+        <a href="<?= URLROOT . '/Home/bookCatalogue' ?>"><button class="portal-button">Book Catalogue</button></a>
+      </div>
+
+      <div class="button-container">
+        <img src="<?= URLROOT . '/public/assets/call.png' ?>" class="portal-button-icons" alt="book">
+        <a href="<?= URLROOT . '/ContactUs/' ?>"><button class="portal-button">Contact Librarian</button></a>
+      </div>
+
+      <div class="button-container">
+        <img src="<?= URLROOT . '/public/assets/user.png' ?>" class="portal-button-icons" alt="book">
+        <a href="<?= URLROOT . '/Login' ?>"><button class="portal-button">Login</button></a>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="reference-lib">
+    <a href="<?= URLROOT . '/Home/bookRequest' ?>"><button class="portal-button-req">Request a book</button></a>
+    <div class="portal-otherButtons">
+      <a href="#"><button class="portal-otherButton">Membership Application form</button></a>
+      <a href="#"><button class="portal-otherButton">Membership Renewal Form</button></a>
+      <a href="#"><button class="portal-otherButton">Rules & Regulations</button></a>
+      <a href="#"><button class="portal-otherButton">Location</button></a>
+    </div>
+
+  </div>
 
 </div>
 
-<div class="portal-intro">
-  <p >Welcome to the Chilaw Public Library Online Portal. 
-  The library has more than 10,000 books that you can search and find copies. 
-  If you are a library member you can log in to your dashboard and access many other features. 
-  Contact the Librarian if there is any problem or Technical error.</p>
-</div>
-
-<div class="portal-buttonsContainer">
-  <div class="button-container">
-    <img src="<?= URLROOT . '/public/assets/user.png' ?>" class="portal-button-icons" alt="book">
-    <a href="<?= URLROOT . '/Home/bookCatalogue' ?>"><button class="portal-button">View Book Catalogue</button></a>
-  </div>
-
-  <div class="button-container">
-    <img src="<?= URLROOT . '/public/assets/user.png' ?>" class="portal-button-icons" alt="book">
-    <a href="<?= URLROOT . '/Home/bookRequest' ?>"><button class="portal-button">Request a book</button></a>
-  </div>
-
-  <div class="button-container">
-    <img src="<?= URLROOT . '/public/assets/user.png' ?>" class="portal-button-icons" alt="book">
-    <a href="<?= URLROOT . '/ContactUs/' ?>"><button class="portal-button">Contact Librarian</button></a>
-  </div>
-
-  <div class="button-container">
-    <img src="<?= URLROOT . '/public/assets/user.png' ?>" class="portal-button-icons" alt="book">
-    <a href="<?= URLROOT . '/Login' ?>"><button class="portal-button">Login</button></a>
-  </div>
-</div>
-
-<div class="portal-otherButtons">
-  <h2>More details:</h2>
-  <a href="#"><button class="portal-otherButton">Membership Application form</button></a>
-  <a href="#"><button class="portal-otherButton">Membership Renewal Form</button></a>
-  <a href="#"><button class="portal-otherButton">Rules & Regulations</button></a>
-  <a href="#"><button class="portal-otherButton">Location</button></a>
-</div>
 
     <script>
 
