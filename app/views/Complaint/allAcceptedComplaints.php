4257458b8abc2410c90c545f85250f9ebcf4e767
sandarud99 <2020cs046@stu.ucsc.cmb.ac.lk>
2023-02-25 15:09:43 +0530

    Modified tabs related to allacceptedcomplaints

--- app/views/Complaint/allAcceptedComplaints.php
+++ app/views/Complaint/allAcceptedComplaints.php
@@ -1,3 +1,4 @@
+<!-- <body onload="openComplaints(event, 'P_Complaints')"> -->
 <div class="content">
 
     <h2>
@@ -5,14 +6,12 @@
         <hr class="hr1">
     </h2>
 
-
-
     <div class="tab">
-        <button class="tablinks" onclick="openComplaints(event, 'P_Complaints')">Processing Complaints</button>
+        <button class="tablinks active" onclick="openComplaints(event, 'P_Complaints')">Processing Complaints</button>
         <button class="tablinks" onclick="openComplaints(event, 'R_Complaints')">Resolved Complaints</button>
     </div>
 
-    <div id="P_Complaints" class="tabcontent">
+    <div id="P_Complaints" class="tabcontent ">
         <div class="content-table">
             <table>
                 <thead>
@@ -140,6 +139,9 @@
 
 <script>
     function openComplaints(evt, complaint) {
+        if (!complaint) {
+            complaint = 'P_Complaints';
+        }
         var i, tabcontent, tablinks;
         tabcontent = document.getElementsByClassName("tabcontent");
         for (i = 0; i < tabcontent.length; i++) {
@@ -151,5 +153,44 @@
         }
         document.getElementById(complaint).style.display = "block";
         evt.currentTarget.className += " active";
+
+    // // Get the first tab button and add the "active" class
+        var firstTabButton = document.querySelector(".tablinks");
+        firstTabButton.classList.add("active");
+       
     }
+
+     //To Activate the first tab and its content on page load
+        window.onload = function() {
+        document.getElementsByClassName("tablinks")[0].click();
+    };
+
+
+
+
+    // window.onload = function() {
+    // document.querySelector('.tablinks').click();
+    // };
+
+    // function openComplaints(evt, complaint) {
+    // var i, tabcontent, tablinks;
+
+    // // Hide all tab content
+    // tabcontent = document.getElementsByClassName('tabcontent');
+    // for (i = 0; i < tabcontent.length; i++) {
+    //     tabcontent[i].style.display = 'none';
+    // }
+
+    // // Remove the 'active' class from all tab buttons
+    // tablinks = document.getElementsByClassName('tablinks');
+    // for (i = 0; i < tablinks.length; i++) {
+    //     tablinks[i].classList.remove('active');
+    // }
+
+    // // Show the selected tab content and add the 'active' class to the clicked tab button
+    // document.getElementById(tabName).style.display = 'block';
+    // evt.currentTarget.classList.add('active');
+    // }
 </script>
+
+
