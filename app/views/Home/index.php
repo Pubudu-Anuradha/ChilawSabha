f329d90eef8e382204d019344860b809845cf259
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2023-02-26 23:22:43 +0530

    Restructuring homepage in progress

--- app/views/Home/index.php
+++ app/views/Home/index.php
@@ -1,7 +1,5 @@
 <div class="slideshow">
-
     <img src="" alt="slideshow" id="slideshow">
-
     <button  class="slideshow-btn left-btn" onclick='prev(["<?=URLROOT . "/public/assets/sabha1.jpg"?>","<?=URLROOT . "/public/assets/sabha2.jpg"?>"])'>&#10094;</button>
     <button  class="slideshow-btn right-btn" onclick='next(["<?=URLROOT . "/public/assets/sabha1.jpg"?>","<?=URLROOT . "/public/assets/sabha2.jpg"?>"])'>&#10095;</button>
 
@@ -11,29 +9,52 @@
 
 <div class="page-content">
     <div class="left-content">
-        <div>
-            <button class="add-complaint btn"  onclick='window.location.href="<?=URLROOT . "/Home/Addcomplaint"?>"'> ADD A COMPLAINT </button>    
-        </div>
-        <div class="library-portal">        
-            <button class="library-portal-btn" onclick='window.location.href="<?=URLROOT . "/Home/Portal"?>"'>
-                <img src="<?=URLROOT . "/public/assets/Library.jpeg"?>" alt="Chilaw Library" class="library-img">
-                <a href="#" class="library-txt">Chilaw Public Library</a>
-            </button>
-        </div>
-        <div class="events">
-            <a href="#"><h3>Upcoming Events</h3></a>
-            <hr>
-            <div class="event-item">
-                <p>20 March 2023</p>
-                <hr>
-                <a href="#"><p>CHRISTMAS  CELEBRATIONS</p></a>
+        <div class="emergency-numbers">
+            <a href="<?=URLROOT . "/Home/emergency"?>"><h3>Emergency Contacts</h3></a>
+            <div class="number">
+                <a href="#" class="name">
+                    Hospital/Ambulance hotline
+                </a>
+                <span class="tel">
+                    0717777777
+                </span>
             </div>
-            <div class="event-item">
-                <p>13 February 2023</p>
-                <hr>
-                <a href="#"><p>NEW-YEAR CELEBRATIONS</p></a>
+            <div class="number">
+                <a href="#" class="name">
+                    Fire Department
+                </a>
+                <span class="tel">
+                    0717777777
+                </span>
             </div>
-            <button class="more btn" onclick='window.location.href="<?=URLROOT . "/Posts/Events"?>"'>More</button>
+            <div class="number">
+                <a href="#" class="name">
+                    Madampe Police station
+                </a>
+                <span class="tel">
+                    0717777777
+                </span>
+            </div>
+            <div class="number">
+                <a href="#" class="name">
+                   Other Emergency Contacts
+                </a>
+            </div>
+        </div>
+        <div class="section">
+            <div class="library-portal">        
+                <a class="library-img" href="<?=URLROOT . "/Home/Portal"?>">
+                    <span>
+                        Chilaw Public Library
+                    </span>
+                </a>
+            </div>
+            <div class="contact-us">
+                <button class="add-complaint btn bg-lightblue hover-bg-blue"
+                        onclick='window.location.href="<?=URLROOT . "/Home/Addcomplaint"?>"'>
+                        MAKE A COMPLAINT
+                </button>    
+            </div>
         </div>
     </div>
 
@@ -41,8 +62,22 @@
         .sabha-img{
             background: url("<?=URLROOT . '/public/assets/logo.jpg'?>");
             background-position: center;
-            background-repeat: no-repeat;
+            background-repeat:no-repeat;
+            background-size:contain;
+        }
+        .library-img{
+            display:flex;
+            align-items: center;
+            justify-content: center;
+            background: url("<?=URLROOT . '/public/assets/Library.jpeg'?>");
+            background-position: center;
+            background-repeat: space;
             background-size: contain;
+            width: 100%;
+            height: 17rem;
+            color: #fff !important;
+            font-weight: 800;
+            font-size: large;
         }
     </style>
     <div class="main-content">
@@ -97,6 +132,39 @@
                 </div>
             </div>
         </div>
+        <div class="posts bg-fd-blue">
+            <div class="posts-header">
+                <a href="#"><h2 class="announcement-txt">Services</h2></a>
+                <button class="more btn bg-lightblue hover-bg-blue" onclick='window.location.href="<?=URLROOT . "/Posts/Events"?>"'>More</button>
+            </div>
+            <hr>
+            <div class="post">
+                <div class="sabha-img"></div>
+                <div class="details">
+                    <a class="title" href="#">REGISTER FOR COVID 19 VACCINATIONS</a>
+                    <div class="date">
+                        21 January 2023
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="posts bg-fd-blue">
+            <div class="posts-header">
+                <a href="#"><h2 class="announcement-txt">Projects</h2></a>
+                <button class="more btn bg-lightblue hover-bg-blue" onclick='window.location.href="<?=URLROOT . "/Posts/Events"?>"'>More</button>
+            </div>
+            <hr>
+            <div class="post">
+                <div class="sabha-img"></div>
+                <div class="details">
+                    <a class="title" href="#">REGISTER FOR COVID 19 VACCINATIONS</a>
+                    <div class="date">
+                        21 January 2023
+                    </div>
+                </div>
+            </div>
+        </div>
 
         <div class="about-city">
             <h2>About Chilaw</h2>
@@ -111,50 +179,13 @@
             </p>
             <button class="more btn" onclick='window.location.href="<?=URLROOT . "/AboutCity"?>"'>See More</button>
         </div>
-
-        <div class="projects">
-            <div class="project-header">
-                <a href="#"><h2 class="project-txt">Ongoing Projects</h2></a>
-                <button class="more btn" onclick='window.location.href="<?=URLROOT . "/Posts/Projects"?>"'>More</button>    
-            </div>
-            
-            <hr>
-            <div class="project-items">
-                <div class="project-item">
-                    <img src="<?=URLROOT . "/public/assets/logo.jpg"?>" alt="logo" class="sabha-img">
-                    <a href="#"><h3>UNICEF Project</h3></a>
-                    <hr>
-                    <p>21 January 2023</p>
-                </div>
-                <div class="project-item">
-                    <img src="<?=URLROOT . "/public/assets/logo.jpg"?>" alt="logo" class="sabha-img">
-                    <a href="#"><h3>Wastewater Management Project</h3></a>
-                    <hr>
-                    <p>14 January 2023</p>
-                </div>
-                <div class="project-item">
-                    <img src="<?=URLROOT . "/public/assets/logo.jpg"?>" alt="logo" class="sabha-img">
-                    <a href="#"><h3>Worldbank Project</h3></a>
-                    <hr>
-                    <p>1 February 2023</p>
-                </div>
-            </div>
-
-        </div>
-
-    </div>
-
-    <div class="right-content">
         <div class="ward-map">
             <h3 class="ward-map-txt">WARD MAP</h3>
             <img src="<?=URLROOT . "/public/assets/wardmap.png"?>" alt="" class="ward-map-img">
         </div>
-        <div class="emergency-numbers">
-            <a href="<?=URLROOT . "/Home/emergency"?>"><h3>Emergency Numbers</h3></a>
-            <hr>
-            <a href="#"><p class="emergency-item">Fire & Rescue / Ambulance</p></a>
-            <a href="#"><p class="emergency-item">Other Emergency Numbers</p></a>
-        </div>
+    </div>
+
+    <div class="right-content">
     </div>
 </div>
     <!-- Function to load slideshow -->
@@ -185,5 +216,4 @@
       }
 
       slideshow(["<?=URLROOT . "/public/assets/sabha1.jpg"?>","<?=URLROOT . "/public/assets/sabha2.jpg"?>"]);
-
     </script>
