ea1dba2b02331d8b4b0cc2eea30d8a7383eb987f
Hasala <hasaladissanayake@yahoo.com>
2023-03-02 11:12:47 +0530

    tried of adding pagination function to library

--- app/views/LibraryStaff/Bookcatalog.php
+++ app/views/LibraryStaff/Bookcatalog.php
@@ -130,13 +130,46 @@
                 </tr>
             </table>
         </div>
-        <div class="pagination-bar">
+        <!-- <div class="page-nav">
+            <?php
+            $page = $data['Users']['page'];
+            $size = $page[1];
+            $max = $data['Users']['count'];
+            $page_count = ceil($max / $size);
+            $current = $page[0] / $size;
+            ?>
+            <div class="page-nos">
+                <?php if($current!=0):?>
+                    <a href="<?= URLROOT . "/Admin/Users?page=0&size=$size" ?>" class="page-btn">&lt;&lt;</a>
+                    <a href="<?= URLROOT . "/Admin/Users?page=".($current - 1)."&size=$size" ?>" class="page-btn">&lt;</a>
+                <?php endif; ?>
+                <select name="page" onchange="send()" id="page">
+                    <?php
+                    $i = 0;
+                    for (; $i * $size < $max; $i++) : ?>
+                        <option value="<?= $i ?>" <?= $i==$current?'selected' : ''?>><?= $i + 1 ?></option>
+                    <?php endfor ?>
+                </select>
+                <?php if($current<$page_count-1):?>
+                    <a href="<?= URLROOT . "/Admin/Users?page=" . ($current + 1) . "&size=$size" ?>" class="page-btn">&gt;</a>
+                    <a href="<?= URLROOT . "/Admin/Users?page=" . ($page_count - 1) . "&size=$size" ?>" class="page-btn">&gt;&gt;</a>
+                <?php endif; ?>
+            </div>
+            <div class="page-size">
+                No.of Posts per page : <select name="size" onchange="send()" id="size">
+                    <?php foreach ([10, 25, 50, 100] as $page_size) : ?>
+                        <option value="<?= $page_size ?>" <?= $page_size == $size?'selected':''?>><?= $page_size ?></option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
+        </div> -->
+        <!-- <div class="pagination-bar">
             <div class="pagination-item">1</div>
             <div class="pagination-item"> 2</div>
             <div class="pagination-item">3</div>
             <div class="pagination-item">4</div>
             <div class="pagination-item"> &#62; </div>
-        </div>
+        </div> -->
     </div>
 </div>
 
