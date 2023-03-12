7a2236e5d64e4e828c741b66822c7fb9ffd69af1
Hasala <hasaladissanayake@yahoo.com>
2023-03-12 09:55:53 +0530

    added data viewing queries for lost list,and change state only for lost books

--- app/models/BookModel.php
+++ app/models/BookModel.php
@@ -1,13 +1,13 @@
 <?php
 class BookModel extends Model
 {
-    public function getBooks($state=['Available','Lent'])
+    public function getBooks($state=[1,2])
     {
         if(count($state) == 2){
-            $condidions = ["(s.status='$state[0]' || s.status='$state[1]')"];
+            $condidions = ["(b.state='$state[0]' || b.state='$state[1]')"];
         }
         else{
-            $condidions = ["s.status='$state[0]'"];
+            $condidions = ["b.state='$state[0]'"];
         }
         if (isset($_GET['search']) && !empty($_GET['search'])) {
             $search_term = mysqli_real_escape_string($this->conn, $_GET['search']);
@@ -31,7 +31,13 @@ class BookModel extends Model
 
         $condidions = implode(' && ', $condidions);
 
-        return $this->selectPaginated('book_status s join books b on b.state=s.status_id join category_codes c on b.category_code=c.category_id',
+        if($state[0] == 4){
+            return $this->selectPaginated('category_codes c join books b on b.category_code=c.category_id join lost_books l on b.accession_no=l.accession_no',
+                'b.accession_no as accession_no,b.title as title,b.author as author,b.publisher as publisher, c.category_name as category_name, l.lost_description as lost_description',
+                $condidions);
+        }
+
+        return $this->selectPaginated('books b join category_codes c on b.category_code=c.category_id',
             'b.accession_no as accession_no,b.title as title,b.author as author,b.publisher as publisher, c.category_name as category_name',
             $condidions);
 
@@ -62,5 +68,25 @@ class BookModel extends Model
          b.accession_no as accession_no,b.isbn as isbn,b.price as price,b.pages as pages,b.recieved_date as recieved_date,
          b.recieved_method as recieved_method',
         "b.accession_no=$id");
+    }
+
+    public function changeState($id,$state,$reason=[]){
+        $accession_no = mysqli_real_escape_string($this->conn,$id);
+        if($state == 4) {
+            $reason['accession_no'] = $accession_no;
+            $reason['lost_record_by'] = $_SESSION['user_id'];
+            // var_dump($accession_no);
+            $res = $this -> insert('lost_books',[
+                'accession_no'=>$reason['accession_no'],
+                'lost_description'=>$reason['lost_description'],
+                'lost_record_by'=>$reason['lost_record_by']
+            ]);
+            if($res){
+                return $this->update('books',[
+                    'state'=> 4
+                ],"accession_no='$accession_no'");
+            }
+            return false;
+        }
     }
 }
