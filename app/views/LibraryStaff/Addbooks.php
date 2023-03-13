c64a74275bd2dbb2f12a029d9810b58a80003ad0
Hasala <hasaladissanayake@yahoo.com>
2023-03-13 16:06:02 +0530

    added nested select for selecting book category, used optgroup instead of using two selects,add js func to give error when recieved date is before published date

--- app/views/LibraryStaff/Addbooks.php
+++ app/views/LibraryStaff/Addbooks.php
@@ -22,11 +22,26 @@ $old = $data['old'] ?? false;
                 <?php Text::text('Place of Publication','place_of_publication','place_of_publication','Insert Place of Publication',maxlength:255);?>
                 <?php Time::date('Date of Publication','date_of_publication','date_of_publication',max:Date("Y-m-d"));?>
                 <?php $categories = [];
-                foreach ($data['categories'] as $category) {
-                    $categories[$category['category_id']] = $category['category_name'];
-                }?>
+                    foreach ($data['categories'] as $category) {
+                        $categories[$category['category_id']] = $category['category_name'];
+                    }
+                ?>
+                <div class="input-field">
+                    <label for="sub_category_code">Book Category</label>
+                    <select name="sub_category_code" id="sub_category_code" required>
+                        <?php  foreach($categories as $value=>$name): ?>
+                            <optgroup label="<?=$name?>">
+                                <?php foreach($data['subcategories'] as $subcategory):
+                                    if($subcategory['category_id'] == $value): ?>
+                                        <option value="<?=$subcategory['sub_category_id']?>" >
+                                            <?=$subcategory['sub_category_name']?>
+                                        </option>
+                                <?php endif; endforeach;?>
+                            </optgroup>
+                        <?php endforeach; ?>
+                    </select>
+                </div>
 
-                <?php Group::select('Book Category','category_code',$categories,selected:$old['role'] ?? null);?>
                 <?php Other::number('Accession No', 'accession_no', 'accession_no', placeholder:'Insert Accession No', min:0);?>
                 <?php Text::text('ISBN No','isbn','isbn','Insert ISBN No',maxlength:50);?>
                 <?php Other::number('Price','price','price',placeholder:'Insert Book Price',step:0.01,min:0);?>
@@ -40,3 +55,21 @@ $old = $data['old'] ?? false;
 
     </div>
 </div>
+
+<script>
+    //to compare dates from front end
+    var form = document.getElementsByTagName('form')[0];
+
+    form.addEventListener('submit', function(event){
+        var pubdate = document.getElementById('date_of_publication').value;
+        var recdate = document.getElementById('recieved_date').value;
+
+        if(pubdate>recdate){
+            event.preventDefault();
+            var recdateErr = document.getElementById('recieved_date');
+            //to show the error as default js form errors in browser
+            recdateErr.setCustomValidity('Recieved date must be after or on published date.'); 
+            recdateErr.reportValidity();
+        }
+    });
+</script>
