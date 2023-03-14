b0aeea05dfeb0e1d51207d52f940b40b301ea372
Hasala <hasaladissanayake@yahoo.com>
2023-03-14 23:16:45 +0530

    added select option and related styles to borrow categories pie chart so that dynamicaly chart can be changed. (according to time ranges)

--- app/views/LibraryStaff/Analytics.php
+++ app/views/LibraryStaff/Analytics.php
@@ -3,18 +3,17 @@
         <h2 class="topic">ANALYTICS</h2>
         <div class="chart-area">
             <div class="pie-chart bg-fd-blue">
-                <div>
+                <div class="pie-chart-options">
                   <h2>Most Borrowed Categories</h2>
-                  <form action="#" method="get">
-                    <select name="month" id="month">
+                    <select name="range" id="range" onchange="changeChart()">
                       <option value="all">All</option>
                       <option value="this month">This Month</option>
                       <option value="last month">Last Month</option>
+                      <option value="this year">This Year</option>
                       <option value="last year">Last Year</option>
-
                     </select>
-                  </form>
                 </div>
+                <div id="pie-chart-div"> </div>
                 <canvas id="most-borrowed-pie"></canvas>
             </div>
             <div class="bar-chart-area">
@@ -33,20 +32,49 @@
 
 <script>
 
-
     var xValues = ["Philosophy", "Science", "Literature","Technology","Religion"];
     var yValues = [80, 49, 44, 60, 29];
     var barColors = ["#6D79E7", "#C7B6EC", "#E5DAFB","#16169c"];
+    var range = document.getElementById('range');
+    var chartDiv = document.getElementById('pie-chart-div');
+    var chartCanvas = document.getElementById('most-borrowed-pie');
+    var chartview;
 
 
     document.addEventListener('DOMContentLoaded', function (){
-      doughnutChart(yValues, xValues, 'most-borrowed-pie');
+
+      // need to add db data for this as well
       hBarChart(yValues, xValues, 'most-favourite-bar');
       hBarChart(yValues, xValues, 'most-plan-to-read-bar');
+
+      fetch("<?=URLROOT . '/LibraryStaff/analytics'?>",{
+                method:"POST",
+                headers: {
+                    "Content-type":"application/json"
+                } ,
+                body: JSON.stringify('all') 
+            })
+            .then(response => response.json())
+            .then(response => {
+
+                if(response.length > 0){
+
+                  const categoryNames = response[0].map(item => item.category_name);
+                  const borrowCounts = response[0].map(item => item.borrow_count);
+
+                  chartview = doughnutChart(borrowCounts, categoryNames, 'most-borrowed-pie');
+
+                }
+        })
+        .catch(err => {
+            chartDiv.innerHTML = "No Data Found";
+            chartCanvas.style.display = "none";
+            chartDiv.classList.add("chartDiv");
+            console.log(err);
+        });
     });
 
     function doughnutChart(yval,xval,id){
-      var barColors = ["#6D79E7", "#C7B6EC", "#E5DAFB","lightblue"];
       // Setup Block
       const data = {
           labels: xval,
@@ -88,6 +116,8 @@
         document.getElementById(id),
         config
       );
+
+      return chart;
     }
 
     function hBarChart(yval,xval,id){
@@ -149,6 +179,45 @@
         document.getElementById(id),
         config
       );
+
+      return chart;
+    }
+
+    function changeChart(){
+
+      fetch("<?=URLROOT . '/LibraryStaff/analytics'?>",{
+          method:"POST",
+          headers: {
+              "Content-type":"application/json"
+          },
+          body: JSON.stringify(range.value)    
+      })
+      .then(response => response.json())
+      .then(response => {
+        console.log(response[0]);
+
+          if(response.length > 0){
+
+            const categoryNames = response[0].map(item => item.category_name);
+            const borrowCounts = response[0].map(item => item.borrow_count);
+
+            chartDiv.innerHTML = "";
+            chartCanvas.style.display = "block";
+
+            if(chartview){
+              chartview.destroy();
+            }
+
+            chartview = doughnutChart(borrowCounts, categoryNames, 'most-borrowed-pie');
+
+          }
+      })
+      .catch(err => {
+          chartDiv.innerHTML = "No Data Found";
+          chartCanvas.style.display = "none";
+          chartDiv.classList.add("chartDiv");
+          console.log(err);
+      });
     }
 
 </script>
