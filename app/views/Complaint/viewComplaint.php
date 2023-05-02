b7b32fb06dcd9aa09cfcfd3df8c0145d2061ac30
sandarud99 <2020cs046@stu.ucsc.cmb.ac.lk>
2023-05-02 11:42:58 +0530

    added add-Note button, Accept button and added styles to viewComplaint page

--- app/views/Complaint/viewComplaint.php
+++ app/views/Complaint/viewComplaint.php
@@ -1,34 +1,54 @@
 <div class="content">
-    <h2 class="topic">New Complaint</h2>
+    <h2 class="topic">Complaint Details</h2>
 
     <?php
     [$complaint, $images] = $data['viewComplaint'] ?? [false, false];
     ?>
     <?php
     if (is_array($complaint) && count($complaint) > 0) {
-        $complaintData = $complaint[0]; // Get the first element of the array
+        $complaintData = $complaint[0];
     ?>
         <div class="complaint-info">
-            <div class="complainer-name"><?php "<p>Complaint ID: " . $complaintData['complaint_id'] . "</p>"; ?></div>
-            <div class="email"><?php echo "<p>Complainer Name: " . $complaintData['complainer_name'] . "</p>"; ?></div>
-            <div class="contact-no"><?php echo "<p>Email: " . $complaintData['email'] . "</p>"; ?></div>
-            <div class="complaint-time"><?php echo "<p>Contact No: " . $complaintData['contact_no'] . "</p>"; ?></div>
-            <div class="address"><?php echo "<p>Address: " . $complaintData['address'] . "</p>"; ?></div>
-            <div class="description"><?php echo "<p>Description: " . $complaintData['description'] . "</p>"; ?></div>
-            <div class="category-name"><?php echo "<p>Category: " . $complaintData['category_name'] . "</p>"; ?></div>
-            <div class="complaint-status"><?php echo "<p>Status: " . $complaintData['complaint_status'] . "</p>"; ?></div>
+            <div class="complaint-state">
+                <?php echo "<p class='complaint_label'>Status: <span class='complaint-status " . strtolower($complaintData['complaint_status']) . "'>" . $complaintData['complaint_status'] . "</p>"; ?>
+                <?php if ($complaintData['complaint_state'] == 1) : ?>
+                    <a href="#" class="btn btn accept bg-red white">Accept</a>
+                <?php endif; ?>
+            </div>
+            <div><?php echo "<p class='complaint_label'>Complainer Name: <span class='complaint-data'>" . $complaintData['complainer_name'] . "</p>"; ?></div>
+            <div><?php echo "<p class='complaint_label'>Email: <span class='complaint-data'>" . $complaintData['email'] . "</p>"; ?></div>
+            <div><?php echo "<p class='complaint_label'>Mobile No: <span class='complaint-data'>" . $complaintData['contact_no'] . "</p>"; ?></div>
+            <div><?php echo "<p class='complaint_label'>Address: <span class='complaint-data'>" . $complaintData['address'] . "</p>"; ?></div>
+            <div><?php echo "<p class='complaint_label'>Date: <span class='complaint-data'>" . $complaintData['complaint_time'] . "</p>"; ?></div>
+            <div><?php echo "<p class='complaint_label'>Category: <span class='complaint-data'>" . $complaintData['category_name'] . "</p>"; ?></div>
+            <div>
+                <p class="complaint_label">Description:</p><textarea readonly class="complaint-data des" name="description" rows="5" cols="10"><?php echo  $complaintData['description']; ?></textarea>
+            </div>
+
         </div>
     <?php
     } else {
         echo "No complaints found.";
     }
     ?>
-    <?php var_dump($images); ?>
+    <div class="topic-img">
+        <h2 class="topic">Photos</h2>
+    </div>
+    <div class="complaint-photos">
+        <?php foreach ($images as $image) : ?>
+            <!-- image path -->
+            <img src="<?= URLROOT . '/Access/confidential/Complaint/' . $image['name'] ?>" alt="<?= $image['orig'] ?>" width="200px">
+        <?php endforeach; ?>
+    </div>
+
+    <div class="notes">
+        <div class="topic-note" style="display: <?php echo ($complaintData['handle_by'] == $_SESSION['user_id'] || $complaintData['complaint_state'] == '1') ? 'flex' : 'none'; ?>; align-items: center;">
+            <h2 class="topic" style="margin-right: 10px;">Notes</h2>
+            <?php if ($complaintData['handle_by'] == $_SESSION['user_id'] || $complaintData['complaint_state'] == 1) : ?>
+                <a href="#" class="btn btn accept bg-green white">Add Note</a>
+            <?php endif; ?>
+        </div>
+    </div>
 
 
-    <?php foreach ($images as $image) : ?>
-
-        <!-- path is important -->
-        <img src="<?= URLROOT . '/Access/confidential/Complaint/' . $image['name'] ?>" alt="<?= $image['orig'] ?>" width="200px">
-    <?php endforeach; ?>
 </div>
