0fdf79ba3c876315a6ec5804c84772ea8ba82268
TharinduSampath2000 <stharindu444@gmail.com>
2023-05-07 13:50:39 +0530

    Modified and finalized library members side bar view

--- app/views/LibraryMember/Sidebar.php
+++ app/views/LibraryMember/Sidebar.php
@@ -3,15 +3,15 @@
         <a href="<?= URLROOT . '/LibraryMember/index' ?>" class="sidebar-drop"><img src="<?=URLROOT . '/public/assets/dashboard-black.png'?>" alt="sidebar-image" class="sidebar-img"><span> DASHBOARD </span></a>
     </div>
     <div class="items">
-        <a href="<?= URLROOT . '/LibraryMember/bookCatalogue' ?>" class="sidebar-drop"><img src="<?=URLROOT . '/public/assets/book.png'?>" alt="sidebar-image" class="sidebar-img"><span> BOOK CATALOGUE </span></a>
+        <a href="<?= URLROOT . '/LibraryMember/bookCatalog' ?>" class="sidebar-drop"><img src="<?=URLROOT . '/public/assets/book.png'?>" alt="sidebar-image" class="sidebar-img"><span> BOOK CATALOGUE </span></a>
     </div>
     <div class="items">
-        <a href="<?= URLROOT . '/LibraryMember/favourite' ?>" class="sidebar-drop"><img src="<?=URLROOT . '/public/assets/heart.png'?>" alt="sidebar-image" class="sidebar-img"><span> FAVOURITE LIST </span></a>
+        <a href="<?= URLROOT . '/LibraryMember/favourites' ?>" class="sidebar-drop"><img src="<?=URLROOT . '/public/assets/heart.png'?>" alt="sidebar-image" class="sidebar-img"><span> FAVOURITE LIST </span></a>
     </div>
     <div class="items">
-        <a href="<?= URLROOT . '/LibraryMember/planToRead' ?>" class="sidebar-drop"><img src="<?=URLROOT . '/public/assets/goals.png'?>" alt="sidebar-image" class="sidebar-img"><span> PLAN TO READ LIST </span></a>
+        <a href="<?= URLROOT . '/LibraryMember/planToReads' ?>" class="sidebar-drop"><img src="<?=URLROOT . '/public/assets/goals.png'?>" alt="sidebar-image" class="sidebar-img"><span> PLAN TO READ LIST </span></a>
     </div>
     <div class="items">
-        <a href="<?= URLROOT . '/LibraryMember/completed' ?>" class="sidebar-drop"><img src="<?=URLROOT . '/public/assets/completed.png'?>" alt="sidebar-image" class="sidebar-img"><span> COMPLETED LIST </span></a>
+        <a href="<?= URLROOT . '/LibraryMember/completeds' ?>" class="sidebar-drop"><img src="<?=URLROOT . '/public/assets/completed.png'?>" alt="sidebar-image" class="sidebar-img"><span> COMPLETED LIST </span></a>
     </div>
 </div>
